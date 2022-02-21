4093cd6b3e0d73a2955099b8265e39b7fe82f83a
raju.rauniyar <raju.rauniyar@pwc.com>
2022-02-21 18:09:36 +0530

    First

--- folder2/providers.tf
+++ folder2/providers.tf
@@ -1,12 +1,22 @@
 terraform {
+  # Set the terraform required version
+  required_version = ">= 0.14.2"
+
+  # Register common providers
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.66.0"
+    }
+  }
+
+  # Persist state in a storage account
   backend "azurerm" {
   }
 }
 
+# Configure the Azure Provider
 provider "azurerm" {
-  version = ">=2.0"
-  # The "feature" block is required for AzureRM provider 2.x.
+  skip_provider_registration = true
   features {}
 }
-
-
