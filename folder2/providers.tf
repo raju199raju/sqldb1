f155b088d18e66492ee7a15f5489c3b615c37b07
raju.rauniyar <raju.rauniyar@pwc.com>
2022-02-21 18:02:07 +0530

    first commit

--- folder2/providers.tf
+++ folder2/providers.tf
@@ -1,12 +1,12 @@
-
-provider "azurerm" {
-     features {}
-     subscription_id = "060c7910-de04-45a3-9953-ad495ae2be2b"
-     client_id = "97a73dfc-7067-48a3-8404-ae91ca18b0d2"
-     client_secret = "5lU7Q~Sm-BZbVgEO1OAf647UBfrFZkAhMANKK"
-     tenant_id = "1ceb3aa7-356f-4e03-b604-c324db3e3db8"
-     version = "~>2.0"
+terraform {
+  backend "azurerm" {
+  }
 }
 
+provider "azurerm" {
+  version = ">=2.0"
+  # The "feature" block is required for AzureRM provider 2.x.
+  features {}
+}
 
 
